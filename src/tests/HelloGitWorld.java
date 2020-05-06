dc0d38d8415f9e9c37722d6c1975bfcdba27d3e2
isikf <f.isik@live.com>
2020-05-06 16:17:40 +0100

    SmallChanges

--- src/tests/HelloGitWorld.java
+++ src/tests/HelloGitWorld.java
@@ -3,5 +3,7 @@ package tests;
 public class HelloGitWorld {
     public static void main(String[] args) {
         System.out.println("Hello new world again");
+
+        System.out.println("New version with a piece of update");
     }
 }
