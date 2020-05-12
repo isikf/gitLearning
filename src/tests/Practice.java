4fad22cffd130816da6fbaf4eb978fcde6506cb3
isikf <62076587+isikf@users.noreply.github.com>
2020-05-12 16:04:47 +0100

    Update Practice.java
    "change Practice.java"

--- src/tests/Practice.java
+++ src/tests/Practice.java
@@ -2,7 +2,10 @@ package tests;
 
 public class Practice {
     public static void main(String[] args) {
+        
         System.out.println("This is for practice.");
+        
+        System.out.println("This is to see the pull function.");
 
     }
 }
